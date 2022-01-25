bfe14ea3117c9a20ef96d960b0ab71567d8616ed
Nick <nbarudi@outlook.com>
2022-01-25 13:07:05 -0500

    More Changes!

--- lab01/ExampleJava.java
+++ lab01/ExampleJava.java
@@ -1,5 +1,6 @@
 public class ExampleJava {
     public static void main(String args[]){
         System.out.println("Hello Example!");
+        System.out.println("Some new Changes to the Beta Branch");
     }
 }
