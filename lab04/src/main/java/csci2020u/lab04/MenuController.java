a27f8de8ee2212f7b3904b20e0bf1ce41659376a
nbarudi <nbarudi@outlook.com>
2022-02-15 14:11:58 -0500

    Fixed Phone Formatting
    Fixed the fact that I forgot to add string formatting to the Phone# section of the menu

--- lab04/src/main/java/csci2020u/lab04/MenuController.java
+++ lab04/src/main/java/csci2020u/lab04/MenuController.java
@@ -1,9 +1,16 @@
 package csci2020u.lab04;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.apache.commons.validator.routines.DomainValidator;
 import org.apache.commons.validator.routines.EmailValidator;
+
+import javax.swing.event.HyperlinkEvent;
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
 
 public class MenuController {
 
@@ -14,7 +21,8 @@ public class MenuController {
     @FXML private TextField phone;
     @FXML private DatePicker dob;
 
-
+    private String phoneFormat = "(\\d{3})(\\d{3})(\\d+)";
+    private String phoneOutput = "($1)-$2-$3";
 
 
     @FXML
@@ -29,5 +37,11 @@ public class MenuController {
             response.setText("Registered!");
         }
 
+    }
+
+    @FXML
+    public void onPhoneAction(ActionEvent actionEvent) {
+        String fString = phone.getText().replaceFirst(phoneFormat, phoneOutput);
+        phone.setText(fString);
     }
 }
